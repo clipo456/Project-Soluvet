f41373c93c17c56084f62014c3d22023994f31ee
Natalia Botasso <99694968+nabotasso@users.noreply.github.com>
2025-03-08 12:06:40 -0300

    Update 1 Soluvet.sql
    Introdução da tabela agendamentos

--- Soluvet.sql
+++ Soluvet.sql
@@ -43,3 +43,14 @@ CREATE TABLE servicos (
     valor DECIMAL(10,2) NOT NULL
 );
 
+/* TABELA DE AGENDAMENTO */
+CREATE TABLE agendamentos (
+    id_agenda INT AUTO_INCREMENTO PRIMARY KEY UNIQUE,
+    data_agendamento DATE NOT NULL,
+    id_serv INT NOT NULL,
+    FOREIGN KEY (id_serv) REFERENCES servicos(id_serv),
+    id_animal INT NOT NULL,
+    FOREIGN KEY (id_animal) REFERENCES cad_animal(id_animal) ON DELETE CASCADE,
+    id_tutor INT NOT NULL,
+    FOREIGN KEY (id_tutor) REFERENCES cad_tutor(id_tutor) ON DELETE CASCADE,
+);
